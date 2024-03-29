54bed0c3a7d1f19bdd1091309eb5e65273a5b77d
Pelle Bloch Atkinson <stillconnecting@gmail.com>
2024-03-29 22:36:08 +0100

    Added Base Power Up

---
+++ Assets/BasePowerUp.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BasePowerUp : MonoBehaviour
+{
+    public abstract void Activate();
+
+    public abstract void UI();
+
+}
---
+++ Assets/Boost.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boost : BasePowerUp
+{
+   
+
+    public override void Activate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UI()
+    {
+        throw new System.NotImplementedException();
+    }
+}
