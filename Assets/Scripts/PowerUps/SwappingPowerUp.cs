5c95fd398de388568cf2a8c3e4753bbc0ac01370
Thomas <tlindh19@student.aau.dk>
2024-03-30 20:24:39 +0100

    smol change

--- Assets/Scripts/PowerUps/SwappingPowerUp.cs
+++ Assets/Scripts/PowerUps/SwappingPowerUp.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DashSlashFlash : BasePowerUp
+public class SwappingPowerUp : BasePowerUp
 {
-    
     public override void OnActivate(PlayerController pc, Vector2 lookDirection)
     {
-        pc.gameObject.transform.position += new Vector3(lookDirection.x, lookDirection.y, 0) * 1.5f;
-        Destroy(this);
+        
     }
 
     public override string GetName()
     {
-        return "Flash";
+        throw new System.NotImplementedException();
     }
 }
