968dc29f3fe3f3dbf5ba30459a44405e763b951b
Thomas <tlindh19@student.aau.dk>
2024-03-30 17:01:56 +0100

    Powerups can be "picked up"

--- Assets/Scripts/PowerupPickup.cs
+++ Assets/Scripts/PowerupPickup.cs
@@ -2,28 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Random = UnityEngine.Random;
 public class PowerupPickup : MonoBehaviour
 {
     private PlayerController pc;
     private Boost _boostPower;
     private DefinetlyNotAniviaWall _wallPower;
     private GameObject _hitPowerupCube;
-    private List<BasePowerUp> _powerups;
+    
+    [SerializeField] private List<BasePowerUp> _powerups;
+    
+    public BasePowerUp currentPowerUp;
+
     private void Start()
     {
         pc = GetComponent<PlayerController>();
         _boostPower = GetComponent<Boost>();
         _wallPower = GetComponent<DefinetlyNotAniviaWall>();
+        Debug.Log(_powerups.Count);
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "PowerUp")
         {
+            var powerupSelector = Random.Range(0, _powerups.Count);
             pc.hasPowerup = true;
             _hitPowerupCube = col.gameObject;
             //destroy powerup cube
+            currentPowerUp = _powerups[powerupSelector];
             Destroy(col.gameObject);
         }
     }
@@ -34,7 +41,6 @@ public class PowerupPickup : MonoBehaviour
         if (pc.hasPowerup)
         {
             //get the different powerups
-            
             
         }
     }
