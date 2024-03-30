ff0f1a1d3cdd9d5508c131094d36b270e868928c
Pelle Bloch Atkinson <stillconnecting@gmail.com>
2024-03-30 15:05:04 +0100

    Added simple Start Screen

---
+++ Assets/StartScreenHandler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class StartScreenHandler : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<UIDocument>().rootVisualElement.Q<Button>("Start").clicked += StartGame;
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
