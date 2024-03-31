9f9d114a668e9cdaa24792e343a794b39c99d897
JLHammer <jlha19@student.aau.dk>
2024-03-31 12:02:57 +0200

    Fixed audio

--- Assets/Scripts/PowerUps/BombPowerUp.cs
+++ Assets/Scripts/PowerUps/BombPowerUp.cs
@@ -8,7 +8,6 @@ public class BombPowerUp : BasePowerUp
 {
     public override void OnActivate(PlayerController pc, Vector2 lookDirection)
     {
-        FindObjectOfType<AudioManager>().Play("Bomb");
         Instantiate(GetComponent<PrefabHolder>().BombPF,
            pc.transform.position+new Vector3(lookDirection.x,lookDirection.y),
            Quaternion.identity).GetComponent<Projectile>().Setup(lookDirection,pc.gameObject);
